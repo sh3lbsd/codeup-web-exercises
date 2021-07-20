f03ecdb096eba2ac471ff97e75b72dc9a3eaac99
sh3lbsd <shelbyed12@gmail.com>
2021-07-20 11:44:06 -0500

    starting objects exercise

---
+++ js/array_warmup.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// /*
+// TODO: Create a function below, concatFirstAndLast, that
+// takes in an array and returns the first and last inputs
+// concatentated together if both elements are strings.
+// If either the first or last element does is not a string
+// or if the array contains one or fewer elements,
+// return false.
+
+function concatFirstAndLast(input) {
+    var input = ["square", "rectangle", "circle", "triangle"];
+    return input = (array.join(["0" + "3"]));
+
+    console.log(input);
+}
+
+
+//  */
+//     Example input/output...
+//
+//     concatFirstAndLast(['a', 'b', 'c']) // 'ac'
+//     concatFirstAndLast([]) // false
+//     concatFirstAndLast(['23', '12', 'hello']) // '23hello'
+//     concatFirstAndLast([true]) // false
+//     concatFirstAndLast([true, 'bob']) // false
+//  */
+//
--- js/iterating.js
+++ js/iterating.js
@@ -24,10 +24,10 @@ var names = ['Suzy', 'Doozy', 'Dingle', 'Dangle'];
      * individually by
      * accessing each element's index.
      */
-    console.log(names[0])
-    console.log(names[1])
-    console.log(names[2])
-    console.log(names[3])
+    console.log(names[0]);
+    console.log(names[1]);
+    console.log(names[2]);
+    console.log(names[3]);
 
     /**
      * TODO:
@@ -37,13 +37,15 @@ var names = ['Suzy', 'Doozy', 'Dingle', 'Dangle'];
      * array.
      */
 for(var i =0; i < names.length; i += 1) {
-
-} )
+    console.log(names[i]);
+}
     /**
      * TODO:
      * Refactor your above code to use a `forEach` loop
      */
-
+names.forEach(function(name) {
+    console.log(name);
+});
     /**
      * TODO:
      * Create the following three functions, each will
@@ -58,5 +60,21 @@ for(var i =0; i < names.length; i += 1) {
      *  > second([1, 2, 3, 4, 5]) // returns 2
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
+
+    function first(arr) {
+        return arr[0];
+    }
+
+    function second(arr) {
+        return arr[1];
+    }
+
+    function last(arr) {
+        return arr[arr.length -1]
+    }
+
+    console.log(first(nums), 1);
+    console.log(second(nums), 2);
+    console.log(last(nums), 5);
 
 })();
