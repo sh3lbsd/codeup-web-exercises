44b7a5791ae37eb872fde51ca3fcd7ab6b9895aa
sh3lbsd <86627394+sh3lbsd@users.noreply.github.com>
2021-09-10 07:47:51 -0500

    finsished promises js/html

--- js/promises.js
+++ js/promises.js
@@ -1,1 +1,18 @@
-fetch(url, {headers: {'Authorization': 'token YOUR_TOKEN_HERE'}})
+fetch(url, {headers: {'Authorization': 'token  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 \n'}})
+"use strict";
+
+function capturedEvent(event){
+    if(event.type === "PushEvent"){
+        return lastPushInfo.created_at;
+    }else{
+        console.log("Not a Push");
+    }
+}
+function lastPushInfo(username){
+    return fetch(`https://api.github.com/users/${username}/events`, {headers: {'Authorization': 'token' + gitHubAccess}})
+        .then(response =>  response.json())
+        .then(response => response[0].created_at)
+        .catch(console.error)
+}
+
+console.log(lastPushInfo("sh3lbsd"));
